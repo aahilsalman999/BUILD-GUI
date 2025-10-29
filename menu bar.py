741a7f8ab008b5e20ced65a3a8317bda48796a2a
aahilsalman999 <aahilsalman1@hotmail.com>
2025-10-29 16:59:14 +0000

    Add files via upload

--- menu bar.py
+++ menu bar.py
@@ -3,11 +3,23 @@ from tkinter.ttk import *
 window = Tk()
 window.geometry("500x500")
 
-#Creating menu bar
+#Creating main menu bar
 menu_bar = Menu(window)
+window.config(menu = menu_bar)
 file = Menu(menu_bar, tearoff = 0)
 menu_bar.add_cascade(label = "File", menu = file)
-file.add_command(label = "New File", command = None)
 
-window.config(menu = menu_bar)
+#Add a submenu under file
+new_submenu = Menu(file, tearoff=0) 
+
+new_submenu.add_command(label = "New File", command = None)
+new_submenu.add_command(label = "Open File", command = None)
+new_submenu.add_command(label = "Edit File", command = None)
+new_submenu.add_separator()
+new_submenu.add_checkbutton(label = "Check", command = None)
+file.add_cascade(label = "Options",menu = new_submenu)
+Save = Menu(file,tearoff=0 )
+menu_bar.add_cascade(label = "Save", menu = Save)
+
+
 window.mainloop()
